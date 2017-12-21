2d7e659148446588b9225592c6813e827574babc
Christian Cambizaca <christianismypapichulo@gmail.com>
2017-12-21 23:15:51 +0000

    Closed to being finished

--- js/script.js
+++ js/script.js
@@ -7,19 +7,6 @@
         // Create your document ready function. 
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
-        var userChoice = '';
-        var computerChoice = '';
-        var winner = '';
-        var computer = ["rock", "paper","scissors"];
-        $( document ).ready(function() {
-                console.log("here");
-                $( ".Yo" ).click(function() {
-                        
-        var x = Math.floor(Math.random()*3);
-        console.log(x);
-        }); 
-        }); 
-        
 //Class 3:  
         // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
         // Display the computer choice to the output screen
@@ -32,12 +19,62 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
-
-
-
+var random = "";
+var choices = ["rock", "paper", "scissors"];
+var userChoice="";
+var computerChoice="";
+var winner="";
 //FUNCTIONS
 
-
-
 // DOCUMENT READY FUNCTION
+$(document).ready (function(){
+        
+function compchoice(){
+        randomizer();
+        var selection = choices[random];
+        computerChoice = selection; 
+        console.log(computerChoice);
+}
+function randomizer(){
+        random = Math.floor(Math.random()*3);
+}        
+function user(){
+        var selection = $('.box').val();
+        userChoice = selection; 
+        console.log(userChoice);
+}
+function reset(){
+        userChoice = "";
+        computerChoice = "";
+}
+
+
+$('button').click(function(){
+             reset();
+             compchoice();
+             userchoice();
+      if (computerChoice === 'rock' && user === 'paper' || computerChoice === 'paper' && user === 'scissors'|| computerChoice === 'scissors' && user === 'rock'){
+                    alert('You win!');
+                    $("#a").append("<br /> You chose " +userChoice+ "   ");
+                    $("#a").append("Computer chose " +computerChoice+ "  W");
+            } else {
+            if (computerChoice === 'rock' && user === 'scissors' || computerChoice === 'paper' && user === 'rock'|| computerChoice === 'scissors' && user === 'paper'){
+                    alert('You LOSE!');
+                    $("#a").append("<br /> You chose " +userChoice+ "   ");
+                    $("#a").append("Computer chose " +computerChoice+ "  L");
+            } else {
+            if (computerChoice === 'rock' && user === 'rock' || computerChoice === 'paper' && user === 'paper'|| computerChoice === 'scissors' && user === 'scissors'){
+                    alert('You TIE!');
+                    $("#a").append("<br /> You chose " +userChoice+ "   ");
+                    $("#a").append("Computer chose " +computerChoice+ "  T");
+            } else {
+            if (user !== 'rock' || user !== 'paper' || user !== 'scissors') {
+                    alert("Do you not know how to play?");
+            }  
+            }
+            }   
+            }
+    });
+        
+});
 
